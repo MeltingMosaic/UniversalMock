bd930783d9d756b67c86a60708a9d4eaf80a4976
MeltingMosaic <meltingmosaic@noreply.github.com>
2017-04-07 14:24:52 -0700

    Added specs

---
+++ spec/specs/universalMock.spec.ts
@@ -0,0 +1,26 @@
+import "jasmine";
+
+// We cannot use the ES6 import format here since universalMock isn't an ES6 module
+const mock = require("../../src/universalMock");
+
+describe("UniversalMock", function() {
+    it("should work", function() {
+        expect(mock).not.toBe(undefined);
+    });
+    it("should have any property", function() {
+        const someProperty = mock.blah;
+        expect(someProperty).not.toBe(null);
+    });
+    it("should return values that are set", function() {
+        const someValue = "Thing!";
+        mock.randomProperty = someValue;
+        expect(mock.randomProperty).toBe(someValue);
+    });
+    it("should work for functions", function() {
+        mock.someFunction = function() { return "hello!"; };
+        expect(mock.someFunction()).toBe("hello!");
+    });
+    it("should return function objects", function() {
+       expect(typeof (mock.anything)).toBe("function");
+    });
+});
